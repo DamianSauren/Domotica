11c73e107d44597b5c42ee6bb301216b85ab9e6b
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2020-12-01 15:04:11 +0100

    Added nav bar item that points to dashboard + dashboard razor page

--- Domotica/Controllers/HomeController.cs
+++ Domotica/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Domotica.Controllers
 {
@@ -19,6 +20,12 @@ namespace Domotica.Controllers
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Dashboard()
         {
             return View();
         }
