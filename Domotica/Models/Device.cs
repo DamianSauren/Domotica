3eff14f052e716aa661475010687af00533a2e33
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2020-12-03 10:41:18 +0100

    Created device classes without logic

---
+++ Domotica/Models/Device.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domotica.Models
+{
+    public class Device
+    {
+        public String DeviceID { get; set; }
+        public String DeviceName { get; set; }
+        public object DeviceProperties { get; set; }
+        public DeviceCategory DeviceCategory { get; set; }
+        
+        public class DHT
+        {
+
+        }
+
+        public class MotionSensor
+        {
+
+        }
+
+        public class Light
+        {
+
+        }
+    }
+}
