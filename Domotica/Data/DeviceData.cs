2f5f84226809ed7275dd95879b314c2a36b902f2
Damian Sauren <damiansauren@gmail.com>
2020-12-09 10:36:10 +0100

    Added a singleton class to store device data

---
+++ Domotica/Data/DeviceData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domotica.Data
+{
+    public sealed class DeviceData
+    {
+        private static DeviceData instance = null;
+        private static readonly object padlock = new object();
+
+        private DeviceData() { }
+
+        public static DeviceData Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DeviceData();
+                    }
+
+                    return instance;
+                }
+            }
+        }
+    }
+}
