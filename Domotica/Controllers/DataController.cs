9255bd2b67490804c7138c9440467cf859c8081b
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2020-12-15 13:27:40 +0100

    Added Devicedata calls + syntax fixes

--- Domotica/Controllers/DataController.cs
+++ Domotica/Controllers/DataController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using Domotica.Data;
 
 //Author: Owen de Bree
 namespace Domotica.Controllers
@@ -20,11 +21,12 @@ namespace Domotica.Controllers
         public bool IsOn { get; set; }
 
         [HttpPost]
-        public async Task<ActionResult> TempSens(float temperature, string tempId)
+        public async Task<ActionResult> TempSens(string tempId, float temperature)
         {
+            DeviceData.Instance.UpdateTempState(tempId, temperature);
             System.Diagnostics.Debug.WriteLine("temp:" + temperature.ToString());
             await Task.WhenAll(
-                feedHub.Clients.All.SendAsync("newTemperatureData", temperature, tempId)
+                feedHub.Clients.All.SendAsync("newTemperatureData", tempId, temperature)
             );
             
             return StatusCode(200);
@@ -32,10 +34,11 @@ namespace Domotica.Controllers
 
         [HttpPost]
         [ActionName("MotionSens")]
-        public async Task<ActionResult> MotionSensPost(bool isTriggered, uint timeOfTrigger, string motionId)
+        public async Task<ActionResult> MotionSensPost(string motionId, bool isTriggered, uint timeOfTrigger)
         {
+            DeviceData.Instance.UpdateMotionState(motionId, isTriggered, timeOfTrigger);
             await Task.WhenAll(
-                feedHub.Clients.All.SendAsync("newMotionData", isTriggered, timeOfTrigger, motionId)
+                feedHub.Clients.All.SendAsync("newMotionData", motionId, isTriggered, timeOfTrigger)
             );
 
             return StatusCode(200);
@@ -43,13 +46,13 @@ namespace Domotica.Controllers
 
         [HttpPost] // Should receive post: Color code, bool on/off
         [ActionName("ReceiveLight")]
-        public async Task<ActionResult> ReceiveLightPost(string hexColor, bool isOn, string lightId)
+        public async Task<ActionResult> ReceiveLightPost(string lightId, string hexColor, bool isOn)
         {
             HexColor = hexColor;
             IsOn = isOn;
-
+            DeviceData.Instance.UpdateLightState(lightId, hexColor, isOn);
             await Task.WhenAll(
-                feedHub.Clients.All.SendAsync("newLightData", hexColor, isOn, lightId)
+                feedHub.Clients.All.SendAsync("newLightData", lightId, hexColor, isOn)
             );
 
             return StatusCode(200);
